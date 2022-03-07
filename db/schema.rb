5e7766f55ad5943af8135521a3ca1a5c73db16e7
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2022-03-07 19:02:30 +0900

    railsコマンド実行時にYour Yarn packages are out of dateメッセージが表示されたためwebpackerのバージョンを更新

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_070015) do
   enable_extension "plpgsql"
 
   create_table "labels", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
